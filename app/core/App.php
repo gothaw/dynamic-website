279e197ce04e8edfada4ca5254c21e268a884f23
gothaw <rad.soltan@gmail.com>
2019-03-03 21:37:17 +0000

    added mvc folder structure + basic routing

---
+++ app/core/App.php
@@ -0,0 +1,21 @@
+<?php
+
+class App {
+
+    protected $controller = 'home';
+    protected $method = 'index';
+    protected $params = [];
+
+    public function __construct()
+    {
+            $url = $this->parseUrl();
+            print_r($url);
+    }
+
+    public function parseUrl()
+    {
+        if(isset($_GET['url'])){
+            return $url = explode('/',filter_var(rtrim($_GET['url'],'/'),FILTER_SANITIZE_URL));
+        }
+    }
+}
