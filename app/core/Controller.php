9d7416dedf3ee2f38eaa32d6c698d0ee52fa9464
gothaw <rad.soltan@gmail.com>
2019-03-07 22:44:30 +0000

    fixed the bug in app and controller files

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -5,7 +5,7 @@ class Controller{
     public function model($model){
         if(file_exists('../app/models/' . $model . '.php')){
             require_once '../app/models/' . $model . '.php';
-            return $model();
+            return new $model();
         }
     }
     public function view($view, $data=[]){
