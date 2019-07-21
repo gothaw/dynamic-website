6d2da356625ca94d7ffd4f5a773bb5f564dcfa69
gothaw <rad.soltan@gmail.com>
2019-07-21 22:46:06 +0100

    Changed views/admin folder structure.

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -4,89 +4,89 @@
 include("../app/views/_includes/view-error.php");
 switch ($subName) {
     case 'admin-membership':
-        include("../app/views/admin/user-search.php");
+        include("../app/views/admin/_includes/user-search.php");
         break;
     case 'admin-membership/edit':
-        include("../app/views/admin/edit-membership.php");
+        include("../app/views/admin/membership/edit-membership.php");
         break;
     case 'admin-membership/cancel':
-        include("../app/views/admin/delete-item.php");
+        include("../app/views/admin/_includes/delete-item.php");
         break;
     case 'admin-members':
-        include("../app/views/admin/user-search.php");
+        include("../app/views/admin/_includes/user-search.php");
         break;
     case 'admin-members/edit':
-        include("../app/views/admin/edit-user.php");
+        include("../app/views/admin/members/edit-user.php");
         break;
     case 'admin-members/add':
-        include("../app/views/admin/add-user.php");
+        include("../app/views/admin/members/add-user.php");
         break;
     case 'admin-members/delete':
-        include("../app/views/admin/delete-item.php");
+        include("../app/views/admin/_includes/delete-item.php");
         break;
     case 'admin-classes':
-        include("../app/views/admin/admin-classes.php");
+        include("../app/views/admin/classes/admin-classes.php");
         break;
     case 'admin-classes/edit':
-        include("../app/views/admin/edit-class.php");
+        include("../app/views/admin/classes/edit-class.php");
         break;
     case 'admin-classes/add':
-        include("../app/views/admin/add-class.php");
+        include("../app/views/admin/classes/add-class.php");
         break;
     case 'admin-classes/delete':
-        include("../app/views/admin/delete-item.php");
+        include("../app/views/admin/_includes/delete-item.php");
         break;
     case 'admin-coaches':
-        include("../app/views/admin/admin-coaches.php");
+        include("../app/views/admin/coaches/admin-coaches.php");
         break;
     case 'admin-coaches/edit':
-        include("../app/views/admin/edit-coach.php");
+        include("../app/views/admin/coaches/edit-coach.php");
         break;
     case 'admin-coaches/add':
-        include("../app/views/admin/add-coach.php");
+        include("../app/views/admin/coaches/add-coach.php");
         break;
     case 'admin-coaches/delete':
-        include("../app/views/admin/delete-item.php");
+        include("../app/views/admin/_includes/delete-item.php");
         break;
     case 'admin-schedule':
-        include("../app/views/admin/admin-schedule.php");
+        include("../app/views/admin/schedule/admin-schedule.php");
         break;
     case 'admin-schedule/edit':
-        include("../app/views/admin/edit-scheduled-class.php");
+        include("../app/views/admin/schedule/edit-scheduled-class.php");
         break;
     case 'admin-schedule/add':
-        include("../app/views/admin/add-scheduled-class.php");
+        include("../app/views/admin/schedule/add-scheduled-class.php");
         break;
     case 'admin-schedule/delete':
-        include("../app/views/admin/delete-item.php");
+        include("../app/views/admin/_includes/delete-item.php");
         break;
     case 'admin-schedule/users':
-        include("../app/views/admin/users-schedule.php");
+        include("../app/views/admin/schedule/users-schedule.php");
         break;
     case 'admin-schedule/users-delete':
-        include("../app/views/admin/delete-item.php");
+        include("../app/views/admin/_includes/delete-item.php");
         break;
     case 'admin-blog':
-        include("../app/views/admin/admin-blog.php");
+        include("../app/views/admin/blog_posts/admin-blog.php");
         break;
     case 'admin-blog/edit':
-        include("../app/views/admin/edit-post.php");
+        include("../app/views/admin/blog_posts/edit-post.php");
         break;
     case 'admin-blog/add':
-        include("../app/views/admin/add-post.php");
+        include("../app/views/admin/blog_posts/add-post.php");
         break;
     case 'admin-blog/delete':
-        include("../app/views/admin/delete-item.php");
+        include("../app/views/admin/_includes/delete-item.php");
         break;
     case 'admin-blog/comments':
-        include("../app/views/admin/blog-comments.php");
+        include("../app/views/admin/blog_posts/blog-comments.php");
         break;
     case 'admin-blog/comments-edit':
-        include("../app/views/admin/edit-comment.php");
+        include("../app/views/admin/blog_posts/edit-comment.php");
         break;
     case 'admin-blog/comments-delete':
-        include("../app/views/admin/delete-item.php");
+        include("../app/views/admin/_includes/delete-item.php");
         break;
     default:
-        include("../app/views/admin/admin-panel.php");
+        include("../app/views/admin/_includes/admin-panel.php");
 }
