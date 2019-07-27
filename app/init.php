38fc717aefa97326e49510be97d20aeb6ba4baf4
gothaw <rad.soltan@gmail.com>
2019-07-27 20:08:26 +0100

    Removed redundant css files and composer config.

--- app/init.php
+++ app/init.php
@@ -5,7 +5,6 @@ require_once 'functions/functions.php';
 
 // Require config files
 require_once 'config/config.php';
-require_once 'config/composer-config.php';
 
 // Autoload require for core classes
 spl_autoload_register(function ($class) {
